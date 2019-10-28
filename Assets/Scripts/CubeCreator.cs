9ce3302ad68287b3e38470ea020739d77bbb1a86
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2019-10-28 10:31:10 -0500

    ProcMeshDemo1
    Added a Quad and a Cube.
    Left Clicking scales up the quad
    Space moves the Cube to the right

---
+++ Assets/Scripts/CubeCreator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCreator : MonoBehaviour
+{
+     float Xpos1, Xpos2;
+
+    private void Awake()
+    {
+        Xpos2 = 1;
+        MakeMesh(Xpos1,Xpos2);
+    }
+    private void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            Xpos1 += 1;
+            Xpos2 += 1;
+        }
+        MakeMesh(Xpos1, Xpos2);
+    }
+    private void MakeMesh(float Xpos1, float Xpos2)
+    {
+        var mf = GetComponent<MeshFilter>();
+
+        var mesh = new Mesh();
+        mf.mesh = mesh;
+
+        var vertices = new Vector3[8]
+{
+            new Vector3(Xpos1,1,0),
+            new Vector3(Xpos2,1,0),
+            new Vector3(Xpos1,1,1),
+            new Vector3(Xpos2,1,1),
+            new Vector3(Xpos1,2,0),
+            new Vector3(Xpos2,2,0),
+            new Vector3(Xpos1,2,1),
+            new Vector3(Xpos2,2,1)
+};
+        mesh.vertices = vertices;
+
+        var tris = new int[36]
+        {
+            1,2,0,
+            1,3,2,
+            0,4,1,
+            4,5,1,
+            1,5,3,
+            5,7,3,
+            4,0,2,
+            2,6,4,
+            2,3,7,
+            7,6,2,
+            4,6,5,
+            6,7,5
+        };
+        mesh.triangles = tris;
+        mesh.RecalculateNormals();
+    }
+}
---
+++ Assets/Scripts/QuadCreator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadCreator : MonoBehaviour
+{
+    private float multiplier = 1;
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            multiplier += 1;
+        }
+        MakeQuad(multiplier);
+    }
+    private void MakeQuad(float a)
+    {
+        var mf = GetComponent<MeshFilter>();
+
+        var mesh = new Mesh();
+        mf.mesh = mesh;
+
+        var vertices = new Vector3[4]
+        {
+            new Vector3(a,0,a),
+            new Vector3(a,0,-a),
+            new Vector3(-a,0,-a),
+            new Vector3(-a,0,a)
+        };
+        mesh.vertices = vertices;
+
+        var tris = new int[6] { 0, 1, 2, 2, 3, 0 };
+        mesh.triangles = tris;
+        mesh.RecalculateNormals();
+    }
+}
