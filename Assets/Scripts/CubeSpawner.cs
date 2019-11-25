5db271d4422b55e7aecd363e8ede75328fff06a4
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2019-11-25 11:42:15 -0600

    Added Prototype Spawner System and Rubble Prefab

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject CubePrefab;
+
+    void Update()
+    {
+        GameObject CubeClone = Instantiate(CubePrefab);
+        CubeClone.GetComponent<RotatingCube>().SpeedX = Random.Range(-0.5f, 0.5f);
+        CubeClone.GetComponent<RotatingCube>().SpeedZ = Random.Range(-0.5f, 0.5f);
+        CubeClone.GetComponent<RotatingCube>().TurnSpeedX = Random.Range(-0.5f, 0.5f);
+        CubeClone.GetComponent<RotatingCube>().TurnSpeedY = Random.Range(-0.5f, 0.5f);
+        CubeClone.GetComponent<RotatingCube>().TurnSpeedZ = Random.Range(-0.5f, 0.5f);
+    }
+}
--- Assets/Scripts/RotatingCube.cs
+++ Assets/Scripts/RotatingCube.cs
@@ -5,48 +5,53 @@ using UnityEngine;
 
 public class RotatingCube : MonoBehaviour
 {
-    Quaternion Rotation = new Quaternion();
     Vector3[] vertices;
     int[] tris;
+    float temp1, temp2;
+    float[] Xpos = new float[8], Ypos = new float[8], Zpos = new float[8];
+
+    public double TurnSpeedX, TurnSpeedY, TurnSpeedZ;
 
     [SerializeField]
-    private int X, Y, Z;
+    Vector3 Center = new Vector3(0, 7, 0);
+
+    public float SpeedX, SpeedY, SpeedZ, Acceleration;
+
+    private void Awake()
+    {       
+        Xpos[0] = Xpos[2] = Xpos[4] = Xpos[6] = -1;
+        Xpos[1] = Xpos[3] = Xpos[5] = Xpos[7] = 1;
+        Ypos[0] = Ypos[1] = Ypos[2] = Ypos[3] = -1;
+        Ypos[4] = Ypos[5] = Ypos[6] = Ypos[7] = 1;
+        Zpos[0] = Zpos[1] = Zpos[4] = Zpos[5] = -1;
+        Zpos[2] = Zpos[3] = Zpos[6] = Zpos[7] = 1;
+        Invoke("Delete", 3.0f);
+    }
 
     void Update()
     {
-        MakeMesh();
         Rotate();
+        MakeMesh(Center);
+        MoveCenter();
     }
 
-    private void Rotate()
-    {
-        //Quaternion Rotation = Quaternion.Euler(X,Y,Z);
-        //Rotation.eulerAngles = new Vector3(X,Y,Z);
-        //Vector3 center = new Vector3(0, 0, 0);
-        //for (int i = 0; i < vertices.Length; i++)
-        //{
-        //    vertices[i] = Rotation * (vertices[i] - center) + center;
-        //}
-        transform.Rotate(X, Y, Z);
-    }
-
-    void MakeMesh()
+    void MakeMesh(Vector3 center)
     {
         var mf = GetComponent<MeshFilter>();
- 
+
         var mesh = new Mesh();
         mf.mesh = mesh;
 
         vertices = new Vector3[8]
 {
-            new Vector3(-1,-1,-1),
-            new Vector3(1,-1,-1),
-            new Vector3(-1,-1,1),
-            new Vector3(1,-1,1),
-            new Vector3(-1,1,-1),
-            new Vector3(1,1,-1),
-            new Vector3(-1,1,1),
-            new Vector3(1,1,1)
+            new Vector3(center.x+Xpos[0],center.y+Ypos[0],center.z+Zpos[0]),
+            new Vector3(center.x+Xpos[1],center.y+Ypos[1],center.z+Zpos[1]),
+            new Vector3(center.x+Xpos[2],center.y+Ypos[2],center.z+Zpos[2]),
+            new Vector3(center.x+Xpos[3],center.y+Ypos[3],center.z+Zpos[3]),
+            new Vector3(center.x+Xpos[4],center.y+Ypos[4],center.z+Zpos[4]),
+            new Vector3(center.x+Xpos[5],center.y+Ypos[5],center.z+Zpos[5]),
+            new Vector3(center.x+Xpos[6],center.y+Ypos[6],center.z+Zpos[6]),
+            new Vector3(center.x+Xpos[7],center.y+Ypos[7],center.z+Zpos[7])
 };
         mesh.vertices = vertices;
 
@@ -67,14 +72,52 @@ public class RotatingCube : MonoBehaviour
         };
         mesh.triangles = tris;
         mesh.RecalculateNormals();
-
     }
-    void RotateY(Mesh mesh)
+    void Rotate()
     {
-        Vector3 center = (mesh.vertices[0] + mesh.vertices[7])/2;
-        for (int i = 0; i < mesh.vertices.Length; i++)
+        RotateX(TurnSpeedX);
+        RotateY(TurnSpeedY);
+        RotateZ(TurnSpeedZ);
+    }
+    void RotateX(double turnSpeed)
+    {
+        for (int i = 0; i < Ypos.Length; i++)
         {
-
+            temp1 = Ypos[i];
+            temp2 = Zpos[i];
+            Ypos[i] = Convert.ToSingle((temp1) * Math.Cos(turnSpeed) + (temp2) * Math.Sin(turnSpeed));
+            Zpos[i] = Convert.ToSingle((temp2) * Math.Cos(turnSpeed) - (temp1) * Math.Sin(turnSpeed));
         }
+    }
+    void RotateY(double turnSpeed)
+    {
+        for (int i = 0; i < Xpos.Length; i++)
+        {
+            temp1 = Xpos[i];
+            temp2 = Zpos[i];
+            Xpos[i] = Convert.ToSingle((temp1) * Math.Cos(turnSpeed) + (temp2) * Math.Sin(turnSpeed));
+            Zpos[i] = Convert.ToSingle((temp2) * Math.Cos(turnSpeed) - (temp1) * Math.Sin(turnSpeed));
+        }
+    }
+    void RotateZ(double turnSpeed)
+    {
+        for (int i = 0; i < Xpos.Length; i++)
+        {
+            temp1 = Xpos[i];
+            temp2 = Ypos[i];
+            Xpos[i] = Convert.ToSingle((temp1) * Math.Cos(turnSpeed) + (temp2) * Math.Sin(turnSpeed));
+            Ypos[i] = Convert.ToSingle((temp2) * Math.Cos(turnSpeed) - (temp1) * Math.Sin(turnSpeed));
+        }
+    }
+    void MoveCenter()
+    {
+        Center.x += SpeedX;
+        Center.y += SpeedY;
+        Center.z += SpeedZ;
+        SpeedY -= Acceleration;
+    }
+    void Delete()
+    {
+        Destroy(this.gameObject);
     }
 }
